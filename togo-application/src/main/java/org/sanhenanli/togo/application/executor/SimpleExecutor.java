2b047bacef357328cb432ff755986eb37616a4e3
zhouwenxiang <zhouwx@lanxi.com>
2020-11-18 10:16:26 +0800

    executor shutdown hook

--- togo-application/src/main/java/org/sanhenanli/togo/application/executor/SimpleExecutor.java
+++ togo-application/src/main/java/org/sanhenanli/togo/application/executor/SimpleExecutor.java
@@ -4,6 +4,7 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.sanhenanli.togo.network.executor.Executor;
 import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import javax.annotation.PostConstruct;
 import java.time.Duration;
@@ -18,19 +19,27 @@ import java.util.concurrent.*;
 @Slf4j
 public class SimpleExecutor implements Executor {
 
-    private ExecutorService executorService;
-    private ExecutorService highPriorityExecutorService;
+    private ThreadPoolTaskExecutor executorService;
+    private ThreadPoolTaskExecutor highPriorityExecutorService;
     private ScheduledExecutorService scheduledExecutorService;
 
     @PostConstruct
     public void init() {
-        executorService = new ThreadPoolExecutor(
-                30, 100, 1, TimeUnit.MINUTES, new LinkedBlockingQueue<>(10000),
-                new CustomizableThreadFactory("do-push-thread-"), (r, executor) -> log.error("reject task"));
+        executorService = new ThreadPoolTaskExecutor();
+        executorService.setCorePoolSize(30);
+        executorService.setMaxPoolSize(100);
+        executorService.setKeepAliveSeconds(60);
+        executorService.setQueueCapacity(10000);
+        executorService.setThreadNamePrefix("do-push-thread-");
+        executorService.setRejectedExecutionHandler((r, executor) -> log.error("reject task"));
 
-        highPriorityExecutorService = new ThreadPoolExecutor(
-                10, 30, 1, TimeUnit.MINUTES, new LinkedBlockingQueue<>(10000),
-                new CustomizableThreadFactory("start-push-thread-"), (r, executor) -> log.error("reject task"));
+        highPriorityExecutorService = new ThreadPoolTaskExecutor();
+        highPriorityExecutorService.setCorePoolSize(10);
+        highPriorityExecutorService.setMaxPoolSize(30);
+        highPriorityExecutorService.setKeepAliveSeconds(60);
+        highPriorityExecutorService.setQueueCapacity(10000);
+        highPriorityExecutorService.setThreadNamePrefix("start-push-thread-");
+        highPriorityExecutorService.setRejectedExecutionHandler((r, executor) -> log.error("reject task"));
 
         scheduledExecutorService = new ScheduledThreadPoolExecutor(30,
                 new CustomizableThreadFactory("scheduled-push-thread-"), (r, executor) -> log.error("reject task"));
